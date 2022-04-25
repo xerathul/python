8faf3d03e949a54fe07ca6eeabe121b3c876a887
SUL <ini2854@naver.com>
2022-04-25 11:42:34 +0900

    django6 Maria

---
+++ django6Maria/myguest/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Guest(models.Model):
+    #ID 자동생성 방지
+    #myno = models.AutoField(auto_created = True, primary_key = True)
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    regdate = models.DateTimeField()
