f6341e3178ba70b2a20378c7104284b6b0e4d2fd
SUL <ini2854@naver.com>
2022-05-10 18:35:27 +0900

    Consumo_cerveja.csv dataset 회귀분석 예제

--- pypro3/anal6ML/lin_regTest4.py
+++ pypro3/anal6ML/lin_regTest4.py
@@ -7,32 +7,41 @@
 #             Precipitacao (mm) : 강수(mm)
 # label : Consumo de cerveja (litros) - 맥주 소비량(리터) 를 예측하시오
 
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics._regression import r2_score, mean_squared_error
-from statsmodels import api
-import statsmodels
-from matplotlib import pyplot as plt
+from sklearn.metrics import r2_score, mean_squared_error
 
 data = pd.read_csv('../testdata/Consumo_cerveja.csv')
 print(data.head(), data.shape)  #(941, 7)
 print(data.columns)
-
+data.dropna(inplace=True) #dropna를 하지 않으면 모델 생성시 오류
 print()
-x = data[['Temperatura Media (C)','Precipitacao (mm)']]
+
+x = data.loc[:,['Temperatura Media (C)','Precipitacao (mm)']]
 x.columns = ['tempt','rain']
 x['tempt']=x['tempt'].apply(lambda x:x.replace(',','.'))
 x['rain']= x['rain'].apply(lambda x:x.replace(',','.'))
 
 
-print(x[:3], type(x.tempt[0]))
+print(x[:3], type(x))
 y = data[['Consumo de cerveja (litros)']]
 y.columns=['beer']
 print(y[:3], type(y))
 
 #model 생성
 lmodel = LinearRegression().fit(x,y)
-print('회귀계수(slope):',lmodel.coef_)  #[-0.06822828]
-print('회귀계수(intercept):',lmodel.intercept_) #30.098860539622496
+print('회귀계수(slope):',lmodel.coef_)  #[[ 0.80190566 -0.07366404]]
+print('회귀계수(intercept):',lmodel.intercept_) #[8.76264285]
+
+pred = lmodel.predict(x)
+print('예측값:', np.round(pred[:5],2).T, type(pred))   #[[30.65 30.43 28.67 27.9  27.86]]
+print('실제값:', y[:5].T,type(y))  #r2_score: 0.37339445073731026
+
+#성능평가
+print('r2_score:', r2_score(y, pred))
+
+#새로운 데이터로 예측
+new_data= [[28,10],[26,0],[30,0]]
+new_pred = lmodel.predict(new_data)
+print(new_pred)
